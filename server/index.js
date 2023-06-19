961954952d981d80e38a0e1f69dec96e61aad056
SantiRosso <santiagorosso@live.com.ar>
2023-06-19 17:29:17 -0300

    prueba

--- server/index.js
+++ server/index.js
@@ -10,8 +10,20 @@ app.use(bodyParser.json());
 // Ruta para recibir las solicitudes del webhook
 app.post("/", (req, res) => {
   // Aquí puedes realizar acciones con los datos recibidos en el webhook
-  console.log(req.body);
+  // console.log(req.body);
+  const { orderType, tikcer, sl, tp1 } = req.body;
 
+  console.log(orderType, tikcer, sl, tp1);
+  // try {
+  //   let result = await;
+  //   connection.createLimitBuyOrder(tikcer, sl, tp1, 0.9, 2.0, {
+  //     comment: "comm",
+  //     clientId: "TE_GBPUSD_7hyINWqAlE",
+  //   });
+  //   console.log("Trade successful, result code is " + result.stringCode);
+  // } catch (err) {
+  //   console.log("Trade failed with result code " + err.stringCode);
+  // }
   // Enviar una respuesta al servidor que envió el webhook
   // res.send("Webhook recibido correctamente");
 });
